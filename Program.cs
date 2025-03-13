e382d521e568c566250d58021941702632e50e2c
pdnam2003 <nam.pd.2446@aptechlearning.edu.vn>
2025-03-13 11:23:30 +0700

    Add files via upload
    thread

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace thr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Thread t1 = new Thread(() => DoWork("luong 1", 2000));
+            Thread t2 = new Thread(() => DoWork("luong 2", 4000));
+
+            t1.Start();
+            t2.Start();
+
+            t1.Join();
+            t2.Join();
+
+
+            Console.WriteLine("ket thuc!!");
+
+
+        }
+
+        static void DoWork(string name, int delay)
+        {
+            Console.WriteLine($"{name} dang chay ...");
+            Thread.Sleep(delay);
+            Console.WriteLine($"{name} da hoan thanh");
+        }
+
+
+
+
+    }
+}
